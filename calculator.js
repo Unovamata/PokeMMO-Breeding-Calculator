dd4bfdf27e3dacba657c7d19153292d95c867f7d
Bryan <91705840+Unovamata@users.noreply.github.com>
2024-10-15 20:17:24 -0600

    Calculator Logic
    • Added the elements for the IVs, Item, and IV Counter + Nature for the Visual Calculator.

--- calculator.js
+++ calculator.js
@@ -24,13 +24,131 @@ document.addEventListener("DOMContentLoaded", function() {
         powersMenu.style.display = "none";
         pricesMenu.style.display = "block";
     });
-
-    function logClickedElement(event) {
-        console.log("Clicked element:", event.target);
-        console.log("Clicked element ID:", event.target.id);
-        console.log("Clicked element class(es):", event.target.className);
-    }
     
-    // Attach this to the document body or a parent element
-    document.body.addEventListener("click", logClickedElement);
+    const calculateButton = document.getElementById("calculate"),
+          treeContainer = document.getElementById("tree");
+
+    class Node {
+        constructor(value, children, item){
+            this.value = value;
+            this.children = children;
+            this.item = item;
+        }
+
+        GraphParent(){
+            const div = document.createElement("div");
+            const IVs = this.value;
+
+            const itemsAndIVs = CreateItemsAndIVCounter(div, IVs);
+
+            IVs.forEach(function(IV){
+                const box = document.createElement("a");
+                box.classList.add("box");
+
+                switch(IV){
+                    case "Nature":
+                        box.classList.add("nature");
+                        box.textContent = "Nat";
+                    break;
+
+                    case "HP":
+                        box.classList.add("hp");
+                        box.textContent = "HP";
+                    break;
+
+                    case "Attack":
+                        box.classList.add("attack");
+                        box.textContent = "Atk";
+                    break;
+
+                    case "Defense":
+                        box.classList.add("defense");
+                        box.textContent = "Def";
+                    break;
+
+                    case "Sp. Attack":
+                        box.classList.add("spatk");
+                        box.textContent = "SpAtk";
+                    break;
+
+                    case "Sp. Defense":
+                        box.classList.add("spdef");
+                        box.textContent = "SpDef";
+                    break;
+
+                    case "Speed":
+                        box.classList.add("speed");
+                        box.textContent = "Spd";
+                    break;
+                    
+                    default:
+                        console.log(IV)
+                    break;
+                }
+
+                div.appendChild(box);
+            });
+
+            const li = treeContainer.querySelector("li");
+            li.appendChild(div);
+        }
+    }
+
+    function CreateItemsAndIVCounter(div, IVs, itemSrc = ""){
+        var itemBox;
+
+        if (itemSrc != ""){
+            itemBox = document.createElement("a");
+            itemBox.classList.add("box");
+            itemBox.classList.add("item");
+            const img = document.createElement("img");
+
+            itemBox.appendChild(img);
+
+            div.appendChild(itemBox);
+        }
+        
+        const IVCounter = document.createElement("a");
+        IVCounter.classList.add("box");
+        IVCounter.classList.add("ivs");
+        var IVsCount = IVs.length,
+            natureHandle = "";
+
+        if(IVs.includes("Nature")){
+            IVsCount -= 1;
+            natureHandle = "N";
+        } 
+        
+        IVCounter.textContent = `${IVsCount}x31${natureHandle}`;
+
+        div.appendChild(IVCounter);
+
+        return {"items": itemBox, "ivs": IVCounter};
+    }
+
+    const IVsToPass = document.getElementById("IVs").querySelectorAll("input");
+
+    calculateButton.addEventListener("click", function(){
+        treeContainer.innerHTML = "";
+
+        const IVs = []
+
+        IVsToPass.forEach(function(element){
+            if (element.checked) {
+                data = element.dataset.label;
+
+                IVs.push(data);
+            }
+        });
+
+        const parent = new Node(IVs, [], "")
+
+        const ul = document.createElement("ul"),
+              li = document.createElement("li");
+
+        ul.appendChild(li);
+        treeContainer.appendChild(ul);
+
+        parent.GraphParent();
+    });
 });
